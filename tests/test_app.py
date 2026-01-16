733b66a98d49a936c4ef0b28213c47ba9b18cec4
Devansh-Chauhan-GitHub <chauhandevansh9@gmail.com>
2026-01-16 16:02:24 +0530

    Update test to handle CI mode without DB

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,5 +4,6 @@ from app import app
 def test_root_endpoint():
     client = app.test_client()
     response = client.get("/")
+
     assert response.status_code == 200
-    assert b"Hello from GCP Cloud Run" in response.data
+    assert b"CI mode" in response.data
