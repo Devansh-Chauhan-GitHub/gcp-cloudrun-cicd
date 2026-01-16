f2c561fd86eff0d7f1f4229ce88912055b7c069e
Devansh-Chauhan-GitHub <chauhandevansh9@gmail.com>
2026-01-16 15:52:50 +0530

    files added

--- app.py
+++ app.py
@@ -1,13 +1,37 @@
 import os
 from flask import Flask
+import mysql.connector
 
 
 app = Flask(__name__)
 
 
+def get_db_connection():
+    return mysql.connector.connect(
+        host=os.environ.get("DB_HOST"),   # private IP of VM
+        user=os.environ.get("DB_USER"),
+        password=os.environ.get("DB_PASSWORD"),
+        database=os.environ.get("DB_NAME"),
+    )
+
+
 @app.route("/")
-def hello():
-    return "Hello from GCP Cloud Run 🚀"
+def show_users():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT id, name, email FROM users")
+    rows = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+
+    output = "<h1>Users from MySQL</h1><ul>"
+    for row in rows:
+        output += f"<li>{row[0]} - {row[1]} ({row[2]})</li>"
+    output += "</ul>"
+
+    return output
 
 
 if __name__ == "__main__":
