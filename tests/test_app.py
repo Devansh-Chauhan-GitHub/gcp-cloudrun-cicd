e215211d79eb82f0c16c573ad49b16999dde5932
Devansh-Chauhan-GitHub <chauhandevansh9@gmail.com>
2026-01-16 11:14:24 +0530

    files added

---
+++ tests/test_app.py
@@ -0,0 +1,8 @@
+from app import app
+
+def test_root_endpoint():
+    client = app.test_client()
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"Hello from GCP Cloud Run" in response.data
+
